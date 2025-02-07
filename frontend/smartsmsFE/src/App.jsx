f142dc76b4fe32a0acd9d0ca12d7cc66dd2341c9
TeBats <bulataoaeronjay@gmail.com>
2025-02-07 13:06:36 +0800

    add routes

--- frontend/smartsmsFE/src/App.jsx
+++ frontend/smartsmsFE/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom'
+import HomePage from '../routes/HomePage'
+import AdminDashboard from '../routes/AdminDashboard'
+import LoginPage from '../routes/LoginPage'
+import UserRegistration from '../routes/UserRegistration'
 
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/admindashboard' element={<AdminDashboard />} />
+          <Route path='/loginpage' element={<LoginPage />} />
+          <Route path='/user' element={<UserRegistration />} />
+        </Routes>
+      </Router>
     </>
   )
 }
